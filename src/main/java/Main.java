d59307375493489c82c941da3546366af44c23d9
poojanehra <poojanehra444@gmail.com>
2021-04-13 17:24:02 -0400

    latest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 <<<<<<< HEAD
         System.out.println("Hello everyone");
 =======
-        System.out.println("Hello pooja");
+
         System.out.println("Kiran");
 >>>>>>> 89348bbde0e7e9ac8f024a5f2fb1516afd75cef7
     }
