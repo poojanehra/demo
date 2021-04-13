30c67f62e7d240ece202f5b22d9eb73c762519d3
poojanehra <poojanehra444@gmail.com>
2021-04-13 16:04:33 -0400

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        System.out.println("Hello pooja");
         System.out.println("Hello everyone");
     }
 
